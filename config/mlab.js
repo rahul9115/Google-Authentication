fac1aa2f3c7d626ce1c9ca9ec9da50a724ccd9f6
rahul <rahul9115>
2020-06-10 22:55:09 +0530

    Google Authentication

--- config/mlab.js
+++
@@ -1,3 +0,0 @@
-module.exports={
-    mlabconnect:'mongodb+srv://rahul9115:nanaamma@cluster0-d6yk7.mongodb.net/test?retryWrites=true&w=majority'
-};
--- routes/authenticate.js
+++
@@ -1,7 +0,0 @@
-const passport=require('passport');
-module.exports = (app)=>{
-app.get('/auth/google',passport.authenticate('google',{
-    scope:['email','profile']
-}))
-app.get('/auth/google/callback',passport.authenticate('google'));
-}
